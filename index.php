0596e62f2ea8f91e3c73ecbd5c985bc8e1a41cd1
Shahf <s.a.sd@outlook.com>
2024-06-12 13:03:23 +0200

    Merge front-end with back-end code, map structure update
    index.html, styles.css removed, html&css update

--- index.php
+++ index.php
@@ -3,21 +3,53 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>YIIPEPEPE</title>
   <?php include("common/styles.php");?>
+  <title>Voedselbank Maaskantje</title>
 </head>
 <body>
-  <?php
-  session_start();
-  include("common/navbar.php");
-  if($_SESSION["loggedIn"] != true){
-    header("location: account.php");
-  }
-  ?>
-
-  <div>
-    <p>yipe</p>
-  </div>
+<?php
+        session_start();
+        include("common/navbar.php");
+        if($_SESSION["loggedIn"] != true){
+          header("location: account.php");
+        }
+?>
+  <header class="header">
+    <div class="header-container">
+        <img class="logo" alt="logo" src="images/logo_voedselbank.png"/>
+        <span class="header-name" href="index.php">Voedselbank Maaskantje</span>
+    </div>    
+      </header>
+  <main>
+    <section class="section-main">
+        <div class="about-us">
+            <div class="about-us-text-box">
+                <h1 class="heading-primary">
+                Welkom bij Voedselbank Maaskantje!
+                </h1>
+                <p class="short-description">
+                Klik hier om in te loggen.
+                </p>
+                <a href="login/login.php" class="btn btn--outline">Login &rarr;</a>
+            </div>
+        </div>
+    </section>
+  </main>
+  <footer>
+    <div class="grid--2-cols">
+        <ul class="voedselbank-info">
+            <li class="footer-list-item">Voedselbank Maaskantje en Regionaal</li>
+            <li class="footer-list-item">Distributiecentrum</li>
+            <li class="footer-list-item">Archangelkade 11 5271 XV Amsterdam</li>
+            <li class="footer-list-item">E: info.maaskantje@voedselbank.org</li>
+            <li class="footer-list-item">T: 020-63877</li>
+        </ul>
+    </div>
+    <div class="row">
+    <p>Stichting Voedselbank Maaskantje wordt door de belastingdienst aangemerkt als ANBI (Algemeen Nut Beogende Instelling) met RSIN 814542499.
+    IBAN: NL49 INGB 0004237650</p>
+    </div>
+  </footer>
   
 </body>
 </html>
